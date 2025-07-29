efd0d8784cc2cc70cdfa1502a0177e1434386707
JameelTech <abdjameeljain@gmail.com>
2025-07-29 23:58:59 +0800

    cove changes

--- LRN.ExcelETL.Service/Services/ExcelEtlProcessor.cs
+++ LRN.ExcelETL.Service/Services/ExcelEtlProcessor.cs
@@ -98,7 +98,7 @@ namespace LRN.ExcelETL.Service.Services
                         fileDto.FileStatus = (int)FileStatusEnum.ImportInProgresss;
 
                         await _importRepo.UpdateFileAsync(fileDto);
-                        await _importRepo.ProcessImportFilesAsync(fileDto);
+                        //await _importRepo.ProcessImportFilesAsync(fileDto);
 
                         _logger.Info($"Imported {result.ImportedRows} rows to {mapping.TargetTable}.");
                     }

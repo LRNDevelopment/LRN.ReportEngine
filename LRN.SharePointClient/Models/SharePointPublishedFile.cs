129b089db005065fe05196f3df424ca1130bc2e5
LRNDevelopment <jameel@labrevenuenavigator.com>
2026-03-17 00:41:54 +0800

    code changes

---
+++ LRN.SharePointClient/Models/SharePointPublishedFile.cs
@@ -0,0 +1,15 @@
+﻿namespace LRN.SharePointClient.Models;
+
+public sealed class SharePointPublishedFile
+{
+	public string? FileName { get; set; }
+	public string? LocalPath { get; set; }
+	public string? RelativeSharePointPath { get; set; }
+	public string? WebUrl { get; set; }       // driveItem.webUrl
+	public string? ShareLinkUrl { get; set; } // createLink().link.webUrl
+
+	public string? NotificationUrl =>
+		!string.IsNullOrWhiteSpace(ShareLinkUrl) ? ShareLinkUrl :
+		!string.IsNullOrWhiteSpace(WebUrl) ? WebUrl :
+		LocalPath;
+}
